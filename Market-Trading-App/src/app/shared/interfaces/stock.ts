ff60e024306ad0fe89c4c47e6723385dcfa84a1c
davisknight7 <davis.knight@bsu.edu>
2024-02-19 00:05:36 -0500

    fix interface types for string concatenation

--- Market-Trading-App/src/app/shared/interfaces/stock.ts
+++ Market-Trading-App/src/app/shared/interfaces/stock.ts
@@ -1,5 +1,12 @@
 export interface Stock {
   name: string;
-  price: number;
-  percent: number;
+  price: string; //should be number to be type safe and converted elsewhere
+  percent: string; //should be number to be type safe and converted elsewhere
+  test1: string;
+  test2: string;
+  test3: string;
 }
+
+/* probably should keep this type safe from background,
+ and have some shared conversion method which converts
+ to string somewhere when displaying and needing to add a symbol */
