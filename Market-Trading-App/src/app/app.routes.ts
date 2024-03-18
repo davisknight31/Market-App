c7d2c0e632d43cab820c6f72d24308a0eb53e799
davisknight7 <davis.knight@bsu.edu>
2024-03-18 00:53:57 -0400

    finish news cards component and add news page

--- Market-Trading-App/src/app/app.routes.ts
+++ Market-Trading-App/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { DetailsComponent } from './modules/details/details.component';
 import { AboutComponent } from './modules/about/about.component';
+import { NewsComponent } from './modules/news/news.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'details/:stock', component: DetailsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'news', component: NewsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- Market-Trading-App/src/app/modules/news/news.component.ts
+++ Market-Trading-App/src/app/modules/news/news.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { ApiService } from '../../../core/services/api.service';
-import { NewsArticles } from '../../interfaces/newsArticles';
+import { ApiService } from '../../core/services/api.service';
+import { NewsArticles } from '../../shared/interfaces/newsArticles';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-news-card',
+  selector: 'app-news',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './news-card.component.html',
-  styleUrl: './news-card.component.scss',
+  templateUrl: './news.component.html',
+  styleUrl: './news.component.scss',
 })
-export class NewsCardComponent {
+export class NewsComponent {
   newsArticles: NewsArticles;
 
   constructor(private apiService: ApiService) {}
@@ -24,14 +24,5 @@ export class NewsCardComponent {
       this.newsArticles = response;
       console.log(response);
     });
-  }
-
-  truncateText(text: string) {
-    length = 75;
-    if (text.length <= length) {
-      return text;
-    }
-
-    return text.substring(0, length) + '\u2026';
   }
 }
